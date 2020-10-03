1bb7057b08338c0f27197c90f9a4df07b4b0d800
Ryan Skene <rjskene83@gmail.com>
2020-10-03 12:38:36 -0400

    add setup file for rtd build

---
+++ docs/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='rknotdocs',
+    version='0.1',
+    description="My sample package",
+    long_description="",
+    author='TODO',
+    author_email='todo@example.org',
+    license='TODO',
+    packages=['rknotdocs'],
+    zip_safe=False,
+    install_requires=[
+        'Sphinx',
+        'nbsphinx',
+          ],
+)
